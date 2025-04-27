dfb2b9a713ad1de52bd9bcc30890f9feaf2431a0
meyigi <faybmi7@gmail.com>
2025-04-27 21:12:34 +0600

    changed some requirements.txt

--- app/utils/randomizer.py
+++ app/utils/randomizer.py
@@ -2,7 +2,7 @@ import time
 import random
 from typing import Iterable, Optional
 
-def random_delay(min_delay: float, max_delay) -> None:
+def random_delay(min_delay: float, max_delay: float) -> None:
     time.sleep(random.uniform(min_delay, max_delay))
 
 def random_proxy(proxies: Iterable[str]) -> Optional[str]:
